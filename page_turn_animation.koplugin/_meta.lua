f8b05dfaeb7dba39eef9a3a939818052e39062ca
Federico Parada <federico1176@gmail.com>
2026-07-15 13:33:24 -0300

    Create _meta.lua for page turn animation plugin
    Add metadata for page turn animation plugin

---
+++ page_turn_animation.koplugin/_meta.lua
@@ -0,0 +1,8 @@
+local _ = require("gettext")
+return {
+    name = "page_turn_animation",
+    fullname = _("Page Turn Animation"),
+    description = _("An AI-powered improvement of the page turn animation patch."),
+    author = "Federico",
+    version = "0.1",
+}
